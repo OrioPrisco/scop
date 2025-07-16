58d0cd70ef6599351bc484d9bcbb808d451c995d
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2025-07-16 15:42:49 +0200

    learnopengl 1.9.8

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,60 @@ const SCR_HEIGHT: u32 = 600;
 
 #[rustfmt::skip]
 const vertices: &[f32] = &[
-     //positions      //colors        //texture coords
-     0.5,  0.5, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0,
-     0.5, -0.5, 0.0,  0.0, 1.0, 0.0,  1.0, 1.0,
-    -0.5, -0.5, 0.0,  0.0, 0.0, 1.0,  0.0, 1.0,
-    -0.5,  0.5, 0.0,  1.0, 1.0, 1.0,  0.0, 0.0,
+     //positions       //colors          //texture coords
+    -0.5, -0.5, -0.5,  1.0,  1.0,  1.0,  0.0,  0.0,
+     0.5, -0.5, -0.5,  1.0,  1.0,  1.0,  1.0,  0.0,
+     0.5,  0.5, -0.5,  1.0,  1.0,  1.0,  1.0,  1.0,
+     0.5,  0.5, -0.5,  1.0,  1.0,  1.0,  1.0,  1.0,
+    -0.5,  0.5, -0.5,  1.0,  1.0,  1.0,  0.0,  1.0,
+    -0.5, -0.5, -0.5,  1.0,  1.0,  1.0,  0.0,  0.0,
+    -0.5, -0.5,  0.5,  1.0,  1.0,  1.0,  0.0,  0.0,
+     0.5, -0.5,  0.5,  1.0,  1.0,  1.0,  1.0,  0.0,
+     0.5,  0.5,  0.5,  1.0,  1.0,  1.0,  1.0,  1.0,
+     0.5,  0.5,  0.5,  1.0,  1.0,  1.0,  1.0,  1.0,
+    -0.5,  0.5,  0.5,  1.0,  1.0,  1.0,  0.0,  1.0,
+    -0.5, -0.5,  0.5,  1.0,  1.0,  1.0,  0.0,  0.0,
+    -0.5,  0.5,  0.5,  1.0,  1.0,  1.0,  1.0,  0.0,
+    -0.5,  0.5, -0.5,  1.0,  1.0,  1.0,  1.0,  1.0,
+    -0.5, -0.5, -0.5,  1.0,  1.0,  1.0,  0.0,  1.0,
+    -0.5, -0.5, -0.5,  1.0,  1.0,  1.0,  0.0,  1.0,
+    -0.5, -0.5,  0.5,  1.0,  1.0,  1.0,  0.0,  0.0,
+    -0.5,  0.5,  0.5,  1.0,  1.0,  1.0,  1.0,  0.0,
+     0.5,  0.5,  0.5,  1.0,  1.0,  1.0,  1.0,  0.0,
+     0.5,  0.5, -0.5,  1.0,  1.0,  1.0,  1.0,  1.0,
+     0.5, -0.5, -0.5,  1.0,  1.0,  1.0,  0.0,  1.0,
+     0.5, -0.5, -0.5,  1.0,  1.0,  1.0,  0.0,  1.0,
+     0.5, -0.5,  0.5,  1.0,  1.0,  1.0,  0.0,  0.0,
+     0.5,  0.5,  0.5,  1.0,  1.0,  1.0,  1.0,  0.0,
+    -0.5, -0.5, -0.5,  1.0,  1.0,  1.0,  0.0,  1.0,
+     0.5, -0.5, -0.5,  1.0,  1.0,  1.0,  1.0,  1.0,
+     0.5, -0.5,  0.5,  1.0,  1.0,  1.0,  1.0,  0.0,
+     0.5, -0.5,  0.5,  1.0,  1.0,  1.0,  1.0,  0.0,
+    -0.5, -0.5,  0.5,  1.0,  1.0,  1.0,  0.0,  0.0,
+    -0.5, -0.5, -0.5,  1.0,  1.0,  1.0,  0.0,  1.0,
+    -0.5,  0.5, -0.5,  1.0,  1.0,  1.0,  0.0,  1.0,
+     0.5,  0.5, -0.5,  1.0,  1.0,  1.0,  1.0,  1.0,
+     0.5,  0.5,  0.5,  1.0,  1.0,  1.0,  1.0,  0.0,
+     0.5,  0.5,  0.5,  1.0,  1.0,  1.0,  1.0,  0.0,
+    -0.5,  0.5,  0.5,  1.0,  1.0,  1.0,  0.0,  0.0,
+    -0.5,  0.5, -0.5,  1.0,  1.0,  1.0,  0.0,  1.0
+
 ];
+
+#[rustfmt::skip]
+const cube_positions : &[Vector3<f32>] = &[
+    Vector3{x:  0.0, y:  0.0, z:  0.0},
+    Vector3{x:  2.0, y:  5.0, z:-15.0},
+    Vector3{x: -1.5, y: -2.2, z: -2.5},
+    Vector3{x: -3.8, y: -2.0, z:-12.3},
+    Vector3{x:  2.4, y: -0.4, z: -3.5},
+    Vector3{x: -1.7, y:  3.0, z: -7.5},
+    Vector3{x:  1.3, y: -2.0, z: -2.5},
+    Vector3{x:  1.5, y:  2.0, z: -2.5},
+    Vector3{x:  1.5, y:  0.2, z: -1.5},
+    Vector3{x: -1.3, y:  1.0, z: -1.5},
+];
+
 #[rustfmt::skip]
 const indices: [u32; 6] = [
     0, 1, 3,
@@ -85,46 +133,51 @@ fn main() {
         .bind_data_from_path("img/awesomeface.png", &mut active_texture)
         .expect("Cannot load texture");
 
-    let mut projection = Mat4::perspective(90.0, 16.0/9.0, 0.1, 1.0);
+    let view = Mat4::translate(&Vector3 {
+        x: 0.0,
+        y: 0.0,
+        z: -3.0,
+    });
+    let mut projection =
+        Mat4::perspective(45.0, (SCR_WIDTH as f32) / (SCR_HEIGHT as f32), 0.1, 100.0);
+    unsafe { gl::Enable(gl::DEPTH_TEST) };
     while !window.should_close() {
         if let Some((width, height)) = process_events(&mut window, &events) {
-            projection = Mat4::perspective(90.0, (width as f32)/(height as f32), 0.1, 1.0);
+            projection = Mat4::perspective(45.0, (width as f32) / (height as f32), 0.1, 100.0);
         }
 
         unsafe {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0); //safe
-            gl::Clear(gl::COLOR_BUFFER_BIT); //can error on bad bit passed
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT); //can error on bad bit passed
 
             //gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
         };
 
         let time_value = glfw.get_time() as f32;
+
         let green_value = time_value.sin() / 2.0 + 0.5;
-        let trans: Mat4<f32> = Mat4::translate(&Vector3 {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0,
-        }) * Mat4::rotate(
-            &Vector3 {
-                x: 1.0,
-                y: 0.0,
-                z: 0.0,
-            }
-            .normalized(),
-            &time_value,
-        ) * Mat4::translate(&Vector3 {
-            x: 0.0,
-            y: 0.0,
-            z: -0.5,
-        });
         shader_program.use_program();
-        unsafe { shader_program.set_mat(c"transform", &trans) }.unwrap();
+        unsafe { shader_program.set_mat(c"view", &view) }.unwrap();
         unsafe { shader_program.set_mat(c"projection", &projection) }.unwrap();
         //unsafe {shader_program.set4f(c"our_color", 0.0, green_value, 0.0, 1.0)}.unwrap();
         unsafe { shader_program.set_texture(c"texture1", &bound_text) };
         unsafe { shader_program.set_texture(c"texture2", &bound_text2) };
         let bound_vao = BoundVao::new(&mut vao, context);
-        bound_vao.draw_elements();
+        for (index, position) in cube_positions.iter().enumerate() {
+            let model = Mat4::translate(&position)
+                 * Mat4::rotate(
+                    &Vector3 {
+                        x: 1.0,
+                        y: 0.3,
+                        z: 0.5,
+                    }
+                    .normalized(),
+                    (20.0 * index as f32).to_radians(),
+                );
+            unsafe { shader_program.set_mat(c"model", &model) }.unwrap();
+
+            bound_vao.draw_triangles();
+        }
         context = bound_vao.unbind();
 
         window.swap_buffers();
--- src/vertex.glsl
+++ src/vertex.glsl
@@ -6,12 +6,13 @@ layout (location = 2) in vec2 aTexCoord;
 out vec3 vertexColor;
 out vec2 TexCoord;
 
-uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
 uniform mat4 projection;
 
 void main()
 {
-    gl_Position = projection * transform * vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
     vertexColor = aColor;
     TexCoord = aTexCoord;
 }
