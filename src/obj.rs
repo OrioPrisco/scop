4d07a6614e314b1fe2264654283cc37eba09a0e5
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2025-08-01 15:31:13 +0200

    Very minimal .obj parser
    lots of things not handled or not properly handled, but it can parse the
    teapot

--- src/obj.rs
+++ src/obj.rs
@@ -25,6 +25,8 @@ pub enum ErrorType {
     Unsupported(String),
     InvalidEntry(String),
     IndexOutOfBound(isize),
+    InvalidParameter(usize),
+    InvalidParameterNumber,
     InvalidLine,
 }
 
@@ -35,6 +37,8 @@ impl Display for ErrorType {
             ErrorType::Unsupported(t) => write!(f, "Unssuported entry type : '{t}'"),
             ErrorType::InvalidEntry(t) => write!(f, "Invalid entry type : '{t}'"),
             ErrorType::IndexOutOfBound(i) => write!(f, "Index {i} is out of bound"),
+            ErrorType::InvalidParameter(p) => write!(f, "Parameter #{p} is invalid"),
+            ErrorType::InvalidParameterNumber => write!(f, "Invalid number of parameters"),
             ErrorType::InvalidLine => write!(f, "Invalid line"),
         }
     }
@@ -48,6 +52,7 @@ pub struct Vertex {
     pub texture_coordinates: (f32, f32),
 }
 
+#[derive(Debug)]
 pub struct Model {
     pub vertices: Box<[Vertex]>,
     pub indices: Box<[u32]>,
@@ -59,9 +64,11 @@ struct VertexData {
 }
 
 pub fn parse_obj(reader: impl BufRead) -> Result<Model, ParseError> {
-    let positions_color: Vec<VertexData> = Vec::new();
-    let normals: Vec<Vector3<f32>> = Vec::new();
-    let texture_coords: Vec<Vector3<f32>> = Vec::new();
+    use ErrorType::*;
+    let mut positions_color: Vec<VertexData> = Vec::new();
+    let mut normals: Vec<Vector3<f32>> = Vec::new();
+    let mut texture_coords: Vec<Vector3<f32>> = Vec::new();
+    let mut indices: Vec<u32> = Vec::new();
 
     for (index, line) in reader.lines().enumerate() {
         let line = line.map_err(|err| ParseError {
@@ -70,23 +77,91 @@ pub fn parse_obj(reader: impl BufRead) -> Result<Model, ParseError> {
             err_type: ErrorType::IOError(err),
         })?;
         macro_rules! error {
-            ($err:tt) => {
+            ($err:expr) => {
                 ParseError {
                     line: Some(line.clone()),
                     line_no: index,
-                    err_type: ErrorType::$err,
+                    err_type: $err,
                 }
             };
         }
-        let line_type = line.split_once(' ').ok_or(error!(InvalidLine))?;
-        match line_type.0 {
-            "v" => (),
-            "vt" => (),
-            "vn" => (),
-            "f" => (),
-            _ => (),
+        if line.starts_with("#") {
+            continue;
+        }
+        if line.is_empty() {
+            continue;
+        }
+        let (line_type, rest) = line.split_once(' ').ok_or(error!(InvalidLine))?;
+        match line_type {
+            "v" => {
+                let args: Vec<_> = rest
+                    .trim()
+                    .split_whitespace()
+                    .map(|s| s.parse::<f32>())
+                    .collect();
+
+                if let Some(err) = args.iter().enumerate().filter(|r| r.1.is_err()).next() {
+                    return Err(error!(InvalidParameter(err.0)));
+                }
+                let mut iter = args.iter().map(|r| *r.as_ref().unwrap());
+                match args.len() {
+                    3 => positions_color.push(VertexData {
+                        position: Vector3::from_iterator(&mut iter),
+                        color: None,
+                    }), // x y z
+                    4 => {
+                        // x y z w
+                        positions_color.push(VertexData {
+                            position: Vector3::from_iterator(&mut iter),
+                            color: None,
+                        }); // x y z w
+                        eprintln!("{index}: w component ignored");
+                    }
+                    6 => positions_color.push(VertexData {
+                        position: Vector3::from_iterator(&mut iter),
+                        color: Some(Vector3::from_iterator(&mut iter)),
+                    }), // x y z r g b
+                    _ => return Err(error!(InvalidParameterNumber)),
+                }
+            }
+            "vt" => (), // vt u [v, w]
+            "vn" => (), //vn x y z  (may not be unit)
+            "f" => {
+                let args: Vec<_> = rest
+                    .trim()
+                    .split_whitespace()
+                    .map(|s| s.parse::<u32>())
+                    .collect();
+
+                if let Some(err) = args.iter().enumerate().find(|r| match r.1 {
+                    Err(_) => true,
+                    Ok(n) => *n == 0,
+                }) {
+                    return Err(error!(InvalidParameter(err.0)));
+                }
+
+                if args.len() != 3 {
+                    return Err(error!(InvalidParameterNumber));
+                }//maybe support polygonal faces
+                indices.extend(args.iter().map(|r| *r.as_ref().unwrap()-1))
+            }
+            ,  // f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3
+            "g" | "o" | "mtlib" | "usetml" => {
+                eprintln!("{index}:Warning {line_type} is not implemented")
+            }
+            "p" | "l" | "curv" | "curv2D" | "surf" | "s" | "mg" | "parm" | "trim" | "hole"
+            | "scrv" | "sp" | "end" | "con" => return Err(error!(Unsupported(line_type.into()))),
+            _ => return Err(error!(InvalidEntry(line_type.into()))),
         }
     }
+    let vertices : Vec<_> = positions_color.iter().map(|p_c| Vertex {
+        position: p_c.position,
+        color: Vector3::zero(),
+        texture_coordinates: (0.0,0.0),
+    }).collect();
+    Ok(Model {
+        vertices : vertices.into(),
+        indices: indices.into(),
+    })
 
-    todo!();
 }
