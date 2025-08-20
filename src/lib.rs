42ccd36374499de0681803cc3ffb359fe453d35d
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2025-08-20 12:40:15 +0200

    Proper error handling in run

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod obj;
 use glfw::{Action, Context, GlfwReceiver, Key};
 use std::cell::RefCell;
 
+use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 
@@ -19,9 +20,25 @@ use math::vector::Vector3;
 const SCR_WIDTH: u32 = 800;
 const SCR_HEIGHT: u32 = 600;
 
-pub fn run() {
+pub struct Config {
+    path: String,
+}
+
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No query String"),
+        };
+        Ok(Config { path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     use glfw::fail_on_errors;
-    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+    let mut glfw = glfw::init(fail_on_errors!())?;
     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
     glfw.window_hint(glfw::WindowHint::OpenGlProfile(
         glfw::OpenGlProfileHint::Core,
@@ -29,7 +46,7 @@ pub fn run() {
 
     let (mut window, events) = glfw
         .create_window(SCR_WIDTH, SCR_HEIGHT, "scop", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW Window");
+        .ok_or("Could not create window")?;
 
     window.make_current();
     window.set_key_polling(true);
@@ -37,21 +54,16 @@ pub fn run() {
 
     gl::load_with(|symbol| window.get_proc_address(symbol));
 
-    let file = BufReader::new(File::open("teapot.obj").unwrap());
-    let model = obj::parse_obj(file).unwrap();
-    println!(
-        "{} {}",
-        model.vertices.len(),
-        model.indices.iter().max().unwrap()
-    );
+    let file = BufReader::new(File::open(config.path)?);
+    let model = obj::parse_obj(file)?;
 
     let mut context = gl::Context::new();
     let mut texture_contexts = texture::get_contexts();
     let mut active_texture = texture::get_active_context();
 
-    let mut vao = Vao::new().unwrap();
-    let vbo = RefCell::new(Vbo::new().unwrap());
-    let ebo = RefCell::new(Ebo::new().unwrap());
+    let mut vao = Vao::new()?;
+    let vbo = RefCell::new(Vbo::new()?);
+    let ebo = RefCell::new(Ebo::new()?);
 
     let mut bound_vao = BoundVao::new(&mut vao, context);
     bound_vao.bind_vbo(&vbo);
@@ -61,10 +73,10 @@ pub fn run() {
     ebo.borrow_mut().bind_data(&model.indices);
     context = bound_vao.unbind();
 
-    let vertex_shader_id = Shader::from_path("./src/vertex.glsl", gl::VERTEX_SHADER).unwrap();
-    let fragment_shader_id = Shader::from_path("./src/fragment.glsl", gl::FRAGMENT_SHADER).unwrap();
+    let vertex_shader_id = Shader::from_path("./src/vertex.glsl", gl::VERTEX_SHADER)?;
+    let fragment_shader_id = Shader::from_path("./src/fragment.glsl", gl::FRAGMENT_SHADER)?;
 
-    let shader_program = ShaderProgram::new(&vertex_shader_id, &fragment_shader_id).unwrap();
+    let shader_program = ShaderProgram::new(&vertex_shader_id, &fragment_shader_id)?;
     //TODO delete shaders
 
     let texture = Texture::new();
@@ -74,12 +86,8 @@ pub fn run() {
     let bound_text = texture.bind(&mut txt_ctx_0, &mut active_texture);
     let bound_text2 = texture2.bind(&mut txt_ctx_1, &mut active_texture);
 
-    bound_text
-        .bind_data_from_path("img/test.png", &mut active_texture)
-        .expect("Cannot load texture");
-    bound_text2
-        .bind_data_from_path("img/awesomeface.png", &mut active_texture)
-        .expect("Cannot load texture");
+    bound_text.bind_data_from_path("img/test.png", &mut active_texture)?;
+    bound_text2.bind_data_from_path("img/awesomeface.png", &mut active_texture)?;
 
     let mut camera_pos = Vector3 {
         x: 0.0,
@@ -127,15 +135,14 @@ pub fn run() {
 
         let view = Mat4::lookat(camera_pos, camera_target, up);
 
-        let green_value = time_value.sin() / 2.0 + 0.5;
         shader_program.use_program();
-        unsafe { shader_program.set_mat(c"view", &view) }.unwrap();
-        unsafe { shader_program.set_mat(c"projection", &projection) }.unwrap();
-        //unsafe {shader_program.set4f(c"our_color", 0.0, green_value, 0.0, 1.0)}.unwrap();
+        unsafe { shader_program.set_mat(c"view", &view) }.ok_or("Cannot set view uniform")?;
+        unsafe { shader_program.set_mat(c"projection", &projection) }
+            .ok_or("Cannot set projection uniform")?;
         unsafe { shader_program.set_texture(c"texture1", &bound_text) };
         unsafe { shader_program.set_texture(c"texture2", &bound_text2) };
         let bound_vao = BoundVao::new(&mut vao, context);
-        unsafe { shader_program.set_mat(c"model", &model) }.unwrap();
+        unsafe { shader_program.set_mat(c"model", &model) }.ok_or("Cannot set model uniform")?;
 
         bound_vao.draw_elements();
         context = bound_vao.unbind();
@@ -143,6 +150,7 @@ pub fn run() {
         window.swap_buffers();
         glfw.poll_events();
     }
+    Ok(())
 }
 
 fn process_events(events: &GlfwReceiver<(f64, glfw::WindowEvent)>) -> Option<(i32, i32)> {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-use scop::run;
+use std::env;
+use std::process;
+
+use scop::{Config, run};
 
 fn main() {
-    run();
+    let conf = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Error parsing arguments {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(conf) {
+        eprintln!("Application error : {e}");
+        process::exit(1);
+    }
 }
