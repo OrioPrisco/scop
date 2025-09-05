77489714c4b0b65c47149a2b09c3c903284b0f70
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2025-09-05 12:03:47 +0200

    Manually drop errors before exiting
    just to supress valgrind wouldn't do this normally

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ use scop::{Config, run};
 fn main() {
     let conf = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Error parsing arguments {err}");
+        drop(err);
         process::exit(1);
     });
 
     if let Err(e) = run(conf) {
         eprintln!("Application error : {e}");
+        drop(e);
         process::exit(1);
     }
 }
